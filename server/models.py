a33aa4e7646f8e0b0973ac0f50056244863cb96e
dorinekiplagat90 <dorinekiplagat90@gmail.com>
2026-01-08 21:38:28 +0300

    added a file

--- server/models.py
+++ server/models.py
@@ -1,45 +1,62 @@
-# server/models.py
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-metadata = MetaData(
-    naming_convention={
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    }
-)
+# Naming convention for migrations
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
 db = SQLAlchemy(metadata=metadata)
 
 
 class Employee(db.Model):
-    __tablename__ = "employees"
+    __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
-    def __repr__(self):
-        return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
+    # One-to-many: Employee has many Reviews
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
 
-
-class Onboarding(db.Model):
-    __tablename__ = "onboardings"
-
-    id = db.Column(db.Integer, primary_key=True)
-    orientation = db.Column(db.DateTime)
-    forms_complete = db.Column(db.Boolean, default=False)
+    # One-to-one: Employee has one Onboarding
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
+        return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
 
 class Review(db.Model):
-    __tablename__ = "reviews"
+    __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
 
+    # Foreign key to Employee
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship back to Employee
+    employee = db.relationship('Employee', back_populates="reviews")
+
     def __repr__(self):
-        return f"<Review {self.id}, {self.year}, {self.summary}>"
+        return f'<Review {self.id}, {self.year}, {self.summary}>'
+
+
+class Onboarding(db.Model):
+    __tablename__ = 'onboardings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    orientation = db.Column(db.DateTime)
+    forms_complete = db.Column(db.Boolean, default=False)
+
+    # Foreign key to Employee
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship back to Employee
+    employee = db.relationship('Employee', back_populates='onboarding')
+
+    def __repr__(self):
+        return f'<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>'
